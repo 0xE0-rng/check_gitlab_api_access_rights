83ddc032f454c110134527a2102d2e108d16aa12
0xE0-rng <14735397+0xE0-rng@users.noreply.github.com>
2022-10-04 08:57:28 +0100

    Create api_access_check.py

---
+++ api_access_check.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+
+
+test_endpoints = ["personal_access_tokens",
+                  "users",
+                  # using random file and main
+                  "projects/1/repository/files/placeholder?ref=main",
+                  # using a random username
+                  "projects?sudo=random_user"]
+
+api_levels=("api/read_api",
+			"read_user",
+			"read_repository",
+			"sudo")
+
+
+if len(sys.argv) < 3:
+    print("Missing Arguments: python3 api_access_check.py [base_url] [api_key]")
+    exit()
+
+base_url = sys.argv[1]
+token = sys.argv[2]
+
+
+for i, endpoint in enumerate(test_endpoints):
+    url = f'http://{base_url}/api/v4/{endpoint}?private_token={token}'
+    ret = requests.get(url, params={'private_token': token})
+    try:
+        if 'error' in ret.json().keys():
+            if 'revoked' in ret.json()['error_description']:
+                print('[!] Token was revoked')
+                exit()
+            continue
+    except AttributeError:
+        # we might get a list as a result
+        pass
+    print("[!] API key has permission level:", api_levels[i])
